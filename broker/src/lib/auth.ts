115cbf6a4c756d96eab2972173ffffdebfb0fa34
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-11-27 13:59:26 +0100

    refactor auth.ts file with the new dialect import

--- broker/src/lib/auth.ts
+++ broker/src/lib/auth.ts
@@ -1,40 +1,9 @@
 import { betterAuth } from 'better-auth';
-import { MssqlDialect } from 'kysely';
-import * as Tedious from 'tedious';
-import * as Tarn from 'tarn';
 import { sveltekitCookies } from 'better-auth/svelte-kit';
 import { getRequestEvent } from '$app/server';
+import { dialect } from './dataAccess/database';
 
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
-
-const dialect = new MssqlDialect({
-	tarn: {
-		...Tarn,
-		options: {
-			min: 0,
-			max: 10
-		}
-	},
-	tedious: {
-		...Tedious,
-		connectionFactory: () =>
-			new Tedious.Connection({
-				authentication: {
-					options: {
-						password: 'password123!',
-						userName: 'sa'
-					},
-					type: 'default'
-				},
-				options: {
-					database: 'broker',
-					port: 1433,
-					trustServerCertificate: true
-				},
-				server: 'localhost'
-			})
-	}
-});
 
 export const auth = betterAuth({
 	baseURL: 'http://localhost:5173',
