6d1ea3ff37d04dc0ff8d7760311451e21878a408
DefinitelyUnlikely <martin@kaarjohansson.se>
2025-11-28 09:35:19 +0100

    fix logout, remove cookies on client side as well

--- broker/src/routes/logout/+server.ts
+++ broker/src/routes/logout/+server.ts
@@ -1,19 +1,16 @@
-import { authClient } from '$lib/authClient';
+import { auth } from '$lib/auth';
 import { redirect, type RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ url }) => {
+export const GET: RequestHandler = async ({ url, request }) => {
 	const redirectTo = url.searchParams.get('redirect_uri');
 
+	await auth.api.signOut({
+		headers: request.headers
+	});
+
 	if (redirectTo) {
-		await authClient.signOut({
-			fetchOptions: {
-				onSuccess: () => {
-					redirect(302, redirectTo);
-				}
-			}
-		});
+		redirect(302, redirectTo);
 	}
 
-	await authClient.signOut();
 	redirect(302, '/');
 };
--- client/src/routes/logout/+server.ts
+++ client/src/routes/logout/+server.ts
@@ -1,6 +1,7 @@
 import { redirect, type RequestHandler } from '@sveltejs/kit';
 import { BROKER_URL, BASE_URL } from '$env/static/private';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ cookies }) => {
+	cookies.delete('oauth_id_token', { path: '/' });
 	throw redirect(302, `${BROKER_URL}/logout?redirect_uri=${BASE_URL}`);
 };
